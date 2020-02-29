25e67142e7bf440bd51234c24ee48d1efff5b7e6
samuelrcm2 <samuelrenancm@gmail.com>
2020-02-28 21:41:22 -0300

    Commit message

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -3,10 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import api from "./Axios";
 
+const dados = () => {
+  api.get('/materials/getAllMaterials').then((response) => {
+    if(response.status == 200)
+      console.log(response)
+  }
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <button onClick={api.get('/materials/getAllMaterials')}> TESTE </button>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <button onClick={() => dados()}> TESTE </button>
     </div>
   );
 }
